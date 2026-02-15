6a47d326c89936805eb6599d5a7cab8c51d62608
James Risberg <james.risberg@gmail.com>
2026-02-15 14:44:32 -0500

    v1.3.0: save raw output alongside clean file

--- Formula/shellrecorder.rb
+++ Formula/shellrecorder.rb
@@ -1,8 +1,8 @@
 class Shellrecorder < Formula
   desc "Record terminal sessions to clean text files"
   homepage "https://github.com/jamesrisberg/shellrecorder"
-  url "https://github.com/jamesrisberg/shellrecorder/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "6b8728f25f5d193ffb1532cc9c274721393e98be69001a464ae0aec3c53a3b66"
+  url "https://github.com/jamesrisberg/shellrecorder/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "b793174c83e34de693f6203fd16bee302780637f1f7ae51ca54f1bfa7bd46643"
   license "MIT"
 
   def install
