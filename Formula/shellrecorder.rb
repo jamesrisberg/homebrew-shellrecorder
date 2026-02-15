b354f30795bdcf38f4c10d2cefbb23d89e19c8b9
James Risberg <james.risberg@gmail.com>
2026-02-15 14:12:56 -0500

    v1.2.0: default save dir ~/.shellrecorder/recordings

--- Formula/shellrecorder.rb
+++ Formula/shellrecorder.rb
@@ -1,8 +1,8 @@
 class Shellrecorder < Formula
   desc "Record terminal sessions to clean text files"
   homepage "https://github.com/jamesrisberg/shellrecorder"
-  url "https://github.com/jamesrisberg/shellrecorder/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "89b69c7e70aaf84d1ae276a9f77b32d44429fcfb591742017da0f7513759f51c"
+  url "https://github.com/jamesrisberg/shellrecorder/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "6b8728f25f5d193ffb1532cc9c274721393e98be69001a464ae0aec3c53a3b66"
   license "MIT"
 
   def install
