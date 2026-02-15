88713f43ae49a2e001c9561995efe7e86dc426de
James Risberg <james.risberg@gmail.com>
2026-02-15 14:01:07 -0500

    Fix SHA256 checksum for v1.0.0

--- Formula/shellrecorder.rb
+++ Formula/shellrecorder.rb
@@ -2,7 +2,7 @@ class Shellrecorder < Formula
   desc "Record terminal sessions to clean text files"
   homepage "https://github.com/jamesrisberg/shellrecorder"
   url "https://github.com/jamesrisberg/shellrecorder/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "b19294f3f0036a50205ca4adfeedf7ca6b8e50e232272f51389ae02ccea22632"
+  sha256 "fdfada1a9443c982eefa0b8526b55388a1ee2f4b2490db50fe471288f73ecec3"
   license "MIT"
 
   def install
